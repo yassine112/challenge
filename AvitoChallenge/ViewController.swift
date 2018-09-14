5e155e449956059292cd3237cb471e0cf04b3702
Yassine EL HALAOUI <yassine.elhalaoui@fly.fr>
2018-09-14 16:26:02 +0100

    First commit

---
+++ AvitoChallenge/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  AvitoChallenge
+//
+//  Created by Fly on 14/09/2018.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
